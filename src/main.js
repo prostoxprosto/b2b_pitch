44f1a0877177d0e65037f6f8e0134aaffcbb065f
Alexei Chepurin <brain@Brain.local>
2023-12-31 15:37:35 +0300

    init

---
+++ src/main.js
@@ -0,0 +1,56 @@
+import { Telegraf, session } from 'telegraf' 
+import { message } from 'telegraf/filters'
+import {code} from 'telegraf/format'
+import config from 'config'
+import { ogg } from './ogg.js'
+import { openai } from './openai.js'
+import { removeFile } from './utils.js'
+import { initCommand, processTextToChat, INITIAL_SESSION } from './logic.js'
+
+console.log(config.get('TEST_ENV'))
+
+
+const bot = new Telegraf(config.get('TELEGRAM_TOKEN'))
+
+// говорим боту, чтобы он использовал session
+bot.use(session()) 
+
+// при вызове команды new и start бот регистрирует новую беседу,
+// новый контекст
+bot.command('new', initCommand)
+bot.command('start', initCommand)
+
+bot.on(message('voice'), async (ctx) => {
+    // если сессия не определилась, создаем новую
+    ctx.session ??=INITIAL_SESSION
+    try {
+        await ctx.reply(code('Сообщение принял. Жду ответ от сервера...'))
+        const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
+        const userId = String(ctx.message.from.id)
+        const oggPath = await ogg.create(link.href, userId)
+        const mp3Path = await ogg.toMp3(oggPath, userId)
+        removeFile(oggPath)
+        const text = await openai.transcription(mp3Path)
+        await ctx.reply(code(`Ваш запрос: ${text}`))
+        await processTextToChat(ctx, text)
+      } catch (e) {
+        console.log(`Error while voice message`, e.message)
+      }
+})
+
+bot.on(message('text'), async (ctx) => {
+    ctx.session ??= INITIAL_SESSION
+    try {
+      await ctx.reply(code('Сообщение принял. Жду ответ от сервера...'))
+      await processTextToChat(ctx, ctx.message.text)
+    } catch (e) {
+      console.log(`Error while voice message`, e.message)
+    }
+})
+
+
+bot.launch()
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
+ 
